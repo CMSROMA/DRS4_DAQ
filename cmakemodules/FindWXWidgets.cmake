d9eda30368e5c1db770b3f1ca076b126cdfbf0f7
StrahinjaLukic <strahinja.lukic@cern.ch>
2017-04-13 15:51:10 -0500

    Updating build system with macros to find root.

--- cmakemodules/FindWXWidgets.cmake
+++ cmakemodules/FindWXWidgets.cmake
@@ -17,17 +17,16 @@ IF( WXCONFIG_DIR )
     SET( WXCONFIG ${WXCONFIG_DIR}/wx-config )
 ENDIF()
 
-find_library( WXCORE_LIB wx_gtk2u_core-3.0 ${WXWidgets_ROOT}/lib )
-
-if( (NOT WXCONFIG) OR (NOT WXCORE_LIB) )
+if( (NOT WXCONFIG) )
   message( FATAL_ERROR "Cannot find WXWidgets. If WXWidgets is present on the system,\
   specify the location where it is installed using variable WXWidgets_DIR.")
 endif()
 
 execute_process(COMMAND ${WXCONFIG} --cxxflags OUTPUT_VARIABLE WXFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND ${WXCONFIG} --libs OUTPUT_VARIABLE WXLIBS OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 # ---------- final checking ---------------------------------------------------
 INCLUDE( FindPackageHandleStandardArgs )
 # set WXWidgets_FOUND to TRUE if all listed variables are TRUE and not empty
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( WXWidgets DEFAULT_MSG WXWidgets_ROOT WXCORE_LIB )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( WXWidgets DEFAULT_MSG WXWidgets_ROOT WXLIBS )
 
